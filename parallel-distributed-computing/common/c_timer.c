f9d1abd05596b37872870076b32502ac5c9ae2e6
giuliano <giuliano.aiello.97@gmail.com>
2024-10-11 19:23:16 +0200

    refactor and feature refactor: project structure feat: add c_timer

---
+++ parallel-distributed-computing/common/c_timer.c
@@ -0,0 +1,20 @@
+//
+//  c_timer.c
+//  parallel-distributed-computing
+//
+//  Created by Giuliano Aiello on 11/10/24.
+//
+
+#include "c_timer.h"
+#include <sys/time.h>
+
+double get_cur_time() {
+  struct timeval   tv;
+  struct timezone  tz;
+  double cur_time;
+  
+  gettimeofday(&tv, &tz);
+  cur_time = tv.tv_sec + tv.tv_usec / 1000000.0;
+  
+  return cur_time;
+}
--- parallel-distributed-computing/maxsum/main.c
+++ parallel-distributed-computing/maxsum/main.c
@@ -7,9 +7,12 @@
 
 #include <stdio.h>
 #include <omp.h>
+#include "../common/c_timer.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    double cur_time = get_cur_time();
+
+    printf("Current time is %f\n", cur_time);
+
     return 0;
 }
