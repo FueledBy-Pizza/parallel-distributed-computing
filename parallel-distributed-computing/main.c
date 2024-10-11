0361abc7954eca962436e4bdb59d6d4c816a96d7
giuliano <giuliano.aiello.97@gmail.com>
2024-10-11 17:44:29 +0200

    Initial Commit

---
+++ parallel-distributed-computing/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  parallel-distributed-computing
+//
+//  Created by Giuliano Aiello on 11/10/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
