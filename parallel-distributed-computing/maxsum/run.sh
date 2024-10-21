40a0539e546e61f6a544ffeb1ea3d480181c5e73
giuliano <giuliano.aiello.97@gmail.com>
2024-10-21 21:34:06 +0200

    feat (maxsum/run): add configuration file

--- parallel-distributed-computing/maxsum/run.sh
+++ parallel-distributed-computing/maxsum/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-source config.sh
-
 if [ ! -x ./build/maxsum.out ]; then
     echo "Executable not found. Exiting."
     exit 1
+fi
+
+if [ -f config.sh ]; then
+    source config.sh
+else
+    MATRIX_ORDER=200   
 fi
 
 ./build/maxsum.out "$MATRIX_ORDER"
