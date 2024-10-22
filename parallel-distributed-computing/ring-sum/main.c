8e6f75e15efef0a7fa153cf0f186362c833642cb
giuliano <giuliano.aiello.97@gmail.com>
2024-10-22 22:24:36 +0200

    feat (ring-sum): add Xcode target attached parallel-distributed-computing.entitlements to it

---
+++ parallel-distributed-computing/ring-sum/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ring-sum
+//
+//  Created by Giuliano Aiello on 22/10/24.
+//
+
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
