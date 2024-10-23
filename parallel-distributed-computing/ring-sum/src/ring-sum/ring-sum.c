57fae42ff19b5ebb97e6718bc3d79afe98b5c1c7
giuliano <giuliano.aiello.97@gmail.com>
2024-10-23 20:45:45 +0200

    WIP (ring-sum)

---
+++ parallel-distributed-computing/ring-sum/src/ring-sum/ring-sum.c
@@ -0,0 +1,8 @@
+//
+//  ring-sum.c
+//  ring-sum
+//
+//  Created by Giuliano Aiello on 23/10/24.
+//
+
+#include "ring-sum.h"
---
+++ parallel-distributed-computing/ring-sum/src/ring-sum/ring-sum.h
@@ -0,0 +1,13 @@
+//
+//  ring-sum.h
+//  ring-sum
+//
+//  Created by Giuliano Aiello on 23/10/24.
+//
+
+#ifndef ring_sum_h
+#define ring_sum_h
+
+#include <stdio.h>
+
+#endif /* ring_sum_h */
