135c0d8dd46724bd5e1d776ea01ac74d6ffc378d
giuliano <giuliano.aiello.97@gmail.com>
2024-10-30 21:46:14 +0100

    fix (ring-sum): algorithm

--- parallel-distributed-computing/ring-sum/src/ring-sum/ring-sum.c
+++ parallel-distributed-computing/ring-sum/src/ring-sum/ring-sum.c
@@ -13,31 +13,31 @@
 
 int ring_sum(int *A, int N){
 
-    int proc_rank = -1, num_proc = -1;
+    int proc_rank, num_proc = -1;
     MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
 
-    int partial_sum_received = 0;
-    int partial_sum_calculated = calculate_partial_sum(A, N);
-    int total_sum = partial_sum_calculated;
+    int partial_sum_incoming = 0;
+    int partial_sum_outgoing = calculate_partial_sum(A, N);
+    int total_sum = partial_sum_outgoing;
 
-    const int TAG = 0;
     int proc_rank_receiver = get_next_proc_rank(proc_rank);
     int proc_rank_sender = get_prev_proc_rank(proc_rank);
 
-    int i;
-    for (i = 0; i < (num_proc - 1); ++i) {
-        if (is_even_proc_rank(proc_rank)) {
-            MPI_Send(&partial_sum_calculated, 1, MPI_INT, proc_rank_receiver, TAG, MPI_COMM_WORLD);
-            MPI_Recv(&partial_sum_received, 1, MPI_INT, proc_rank_sender, TAG, MPI_COMM_WORLD, NULL);
-        } else {
-            MPI_Recv(&partial_sum_received, 1, MPI_INT, proc_rank_sender, TAG, MPI_COMM_WORLD, NULL);
-            MPI_Send(&partial_sum_calculated, 1, MPI_INT, proc_rank_receiver, TAG, MPI_COMM_WORLD);
-        }
-        total_sum += partial_sum_received;
+    const int TAG = 0;
 
-        proc_rank_receiver = get_next_proc_rank(proc_rank_receiver);
-        proc_rank_sender = get_prev_proc_rank(proc_rank_sender);
+    int i;
+    for (i = 0; i < num_proc - 1; ++i) {
+        if (is_even_proc_rank(proc_rank)) {
+            MPI_Send(&partial_sum_outgoing, 1, MPI_INT, proc_rank_receiver, TAG, MPI_COMM_WORLD);
+            MPI_Recv(&partial_sum_incoming, 1, MPI_INT, proc_rank_sender, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        } else {
+            MPI_Recv(&partial_sum_incoming, 1, MPI_INT, proc_rank_sender, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(&partial_sum_outgoing, 1, MPI_INT, proc_rank_receiver, TAG, MPI_COMM_WORLD);
+        }
+
+        total_sum += partial_sum_incoming;
+        partial_sum_outgoing = partial_sum_incoming;
     }
 
     return total_sum;
