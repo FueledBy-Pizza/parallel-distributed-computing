a0c62e62686257909a8d6bc2889e1dce0cb16515
giuliano <giuliano.aiello.97@gmail.com>
2024-11-22 17:29:06 +0100

    feat (hpc-matmatblock init packages)

---
+++ parallel-distributed-computing/hpc/matmatblock/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  matmatblock
+//
+//  Created by Giuliano Aiello on 22/11/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
