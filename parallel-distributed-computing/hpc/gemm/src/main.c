7f667daaeee79f8b97d354f74baa9e0d2ea4e264
giuliano <giuliano.aiello.97@gmail.com>
2024-11-27 19:14:39 +0100

    refactor (hpc): extract function to print_cc

--- parallel-distributed-computing/hpc/gemm/src/main.c
+++ parallel-distributed-computing/hpc/gemm/src/main.c
@@ -13,6 +13,8 @@
 #include "../../../common/matrix/matrix.h"
 #include "../../../common/c_timer/c_timer.h"
 
+void print_cc(void);
+
 int main(int argc, const char * argv[]) {
 
     if (argc < 2) {
@@ -22,23 +24,6 @@ int main(int argc, const char * argv[]) {
 
     int N = atoi(argv[1]);
     int LD = N;
-
-#ifdef __clang__
-    printf("  Compiler: Clang\n");
-    printf("  Version: %d.%d.%d\n", __clang_major__, __clang_minor__, __clang_patchlevel__);
-#endif
-
-#ifdef __GNUC__
-    // Ensure it's not Clang (Clang also defines __GNUC__)
-#ifndef __clang__
-    printf("  Compiler: GCC\n");
-    printf("  Version: %d.%d.%d\n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
-#endif
-#endif
-
-#ifdef __OPTIMIZE__
-    printf("  Optimization: Enabled\n");
-#endif
 
     double *A = (double *) malloc(sizeof(double) * N * LD);
     double *B = (double *) malloc(sizeof(double) * N * LD);
@@ -115,4 +100,23 @@ int main(int argc, const char * argv[]) {
     free(A);
     free(B);
     free(C);
+}
+
+void print_cc(void) {
+    #ifdef __clang__
+        printf("\tCompiler: Clang\n");
+        printf("\tVersion: %d.%d.%d\n", __clang_major__, __clang_minor__, __clang_patchlevel__);
+    #endif
+
+    #ifdef __GNUC__
+        // Ensure it's not Clang (Clang also defines __GNUC__)
+    #ifndef __clang__
+        printf("\tCompiler: GCC\n");
+        printf("\tVersion: %d.%d.%d\n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+    #endif
+    #endif
+
+    #ifdef __OPTIMIZE__
+        printf("\tOptimization: Enabled\n");
+    #endif
 }
--- parallel-distributed-computing/hpc/matmatblock/src/main.c
+++ parallel-distributed-computing/hpc/matmatblock/src/main.c
@@ -12,6 +12,8 @@
 #include "../../../common/matrix/matrix.h"
 #include "../../../common/c_timer/c_timer.h"
 
+void print_cc(void);
+
 int main(int argc, const char * argv[]) {
 
     if (argc < 3) {
@@ -22,23 +24,6 @@ int main(int argc, const char * argv[]) {
     const int N = atoi(argv[1]);
     const int dim_block = atof(argv[2]);
     const int LD = N;
-
-#ifdef __clang__
-    printf("  Compiler: Clang\n");
-    printf("  Version: %d.%d.%d\n", __clang_major__, __clang_minor__, __clang_patchlevel__);
-#endif
-
-#ifdef __GNUC__
-    // Ensure it's not Clang (Clang also defines __GNUC__)
-#ifndef __clang__
-    printf("  Compiler: GCC\n");
-    printf("  Version: %d.%d.%d\n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
-#endif
-#endif
-
-#ifdef __OPTIMIZE__
-    printf("  Optimization: Enabled\n");
-#endif
 
     double *A = (double *) malloc(sizeof(double) * N * LD);
     double *B = (double *) malloc(sizeof(double) * N * LD);
@@ -77,4 +62,23 @@ int main(int argc, const char * argv[]) {
     free(B);
     free(C);
 
+}
+
+void print_cc(void) {
+    #ifdef __clang__
+        printf("\tCompiler: Clang\n");
+        printf("\tVersion: %d.%d.%d\n", __clang_major__, __clang_minor__, __clang_patchlevel__);
+    #endif
+
+    #ifdef __GNUC__
+        // Ensure it's not Clang (Clang also defines __GNUC__)
+    #ifndef __clang__
+        printf("\tCompiler: GCC\n");
+        printf("\tVersion: %d.%d.%d\n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+    #endif
+    #endif
+
+    #ifdef __OPTIMIZE__
+        printf("\tOptimization: Enabled\n");
+    #endif
 }
