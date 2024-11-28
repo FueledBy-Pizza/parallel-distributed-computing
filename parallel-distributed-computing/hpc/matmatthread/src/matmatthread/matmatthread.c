6314c3d490640ae4d79561230254fb9e3f2caf98
giuliano <giuliano.aiello.97@gmail.com>
2024-11-28 13:50:54 +0100

    feat (hpc-matmatthread): add package

--- parallel-distributed-computing/hpc/matmatthread/src/matmatthread/matmatthread.c
+++ parallel-distributed-computing/hpc/matmatthread/src/matmatthread/matmatthread.c
@@ -6,3 +6,7 @@
 //
 
 #include "matmatthread.h"
+
+void matmatthread(int ldA, int ldB, int ldC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3, int NTROW, int NTCOL) {
+    
+}
--- parallel-distributed-computing/hpc/matmatthread/src/matmatthread/matmatthread.h
+++ parallel-distributed-computing/hpc/matmatthread/src/matmatthread/matmatthread.h
@@ -8,6 +8,6 @@
 #ifndef matmatthread_h
 #define matmatthread_h
 
-#include <stdio.h>
+void matmatthread(int ldA, int ldB, int ldC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3, int NTROW, int NTCOL);
 
 #endif /* matmatthread_h */
