d782b151c9fcbedd163b08742d2f6258ca6045da
giuliano <giuliano.aiello.97@gmail.com>
2024-12-07 14:32:39 +0100

    feat (hpc-matmatdist): build communicators (WIP)

--- parallel-distributed-computing/hpc/matmatdist/src/main.c
+++ parallel-distributed-computing/hpc/matmatdist/src/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
+#include <math.h>
 #include "matmatdist/matmatdist.h"
 #include "../../../common/c_timer/c_timer.h"
 
@@ -15,7 +16,7 @@ int main(int argc, char * argv[]) {
     // usare questo programma chiamante per fare test di correttezza per matmatdist
     // SOLO CON GLIGLIE DI PROCESSI (NPROW , NPCOL) = (1,1) e (2,2)
 
-    int i, j, Nglob, Mglob, Pglob, lda, mcm ;
+    int i, j, N1, N2, N3, ld, mcm ;
     int dims[2], period[2], coord[2], TROW, TCOL, rank, size;
     int X, Y, Q, R;
     double *A, *B, *C, *D;
@@ -31,27 +32,31 @@ int main(int argc, char * argv[]) {
     // ATTENZIONE: il prodotto dims[0]*dims[1] deve essere uguale al
     // numero di processi lanciati da mpirun nel file.pbs
     //
-    dims[0] = 1; dims[1] = 2;
-    period[0] = 1; period[1] = 1;
+    dims[0] = 2;
+    dims[1] = 2;
+    int NProw = dims[0];
+    int NPcol = dims[1];
+    period[0] = 1;
+    period[1] = 1;
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, period, 0, &GridCom);
 
     //
     // allocazione dello spazio per i test
     //
-    lda = 6144;
-    A=(double*)malloc(sizeof(double)*lda*lda);
-    B=(double*)malloc(sizeof(double)*lda*lda);
-    C=(double*)malloc(sizeof(double)*lda*lda);
-    D=(double*)malloc(sizeof(double)*lda*lda);
+    ld = 6144;
+    A=(double*)malloc(sizeof(double)*ld*ld);
+    B=(double*)malloc(sizeof(double)*ld*ld);
+    C=(double*)malloc(sizeof(double)*ld*ld);
+    D=(double*)malloc(sizeof(double)*ld*ld);
 
 
     // ==================================================
     // test di correttezza risultati. Verificare solo per griglie di processi (1,1) e (2,2)
     // ==================================================
 
-    Nglob= 2;
-    Mglob= 4;
-    Pglob= 4;
+    N1 = 2;
+    N2 = 4;
+    N3 = 4;
     TROW = 1; TCOL =1 ;
 
     MPI_Cart_coords(GridCom, rank, 2, coord);
@@ -64,49 +69,53 @@ int main(int argc, char * argv[]) {
         X = Y;
         Y = R;
     }
-    mcm = dims[0]*dims[1]/X;
+    mcm = (dims[0] * dims[1]) / X;
 
     //
     // definizione delle matrici di input
     //
-    for (i=0; i<Nglob/dims[0]; i++){
-        for (j=0; j<Mglob/mcm; j++){
-            A[i*lda+j] = coord[0]*dims[1]*Mglob/mcm + coord[1]*Mglob/mcm + i*Mglob+j;
+    for (i=0; i<N1/NProw; i++){
+        for (j=0; j<N2/mcm; j++){
+            A[i*ld+j] = coord[0]*dims[1]*N2/mcm + coord[1]*N2/mcm + i*N2+j;
         }
     }
-    for (i=0; i<Mglob/mcm; i++){
-        for (j=0; j<Pglob/dims[1]; j++){
-            B[i*lda+j] = 10 + coord[0]*dims[1]*Pglob + coord[1]*Pglob/dims[1] + i*Pglob+j;
+    for (i=0; i<N2/mcm; i++){
+        for (j=0; j<N3/NPcol; j++){
+            B[i*ld+j] = 10 + coord[0]*dims[1]*N3 + coord[1]*N3/dims[1] + i*N3+j;
         }
     }
-    for (i=0; i<Nglob/dims[0]; i++){
-        for (j=0; j<Pglob/dims[1]; j++){
-            C[i*lda+j] = 0.0;
+    for (i=0; i<N1/NProw; i++){
+        for (j=0; j<N3/NPcol; j++){
+            C[i*ld+j] = 0.0;
         }
     }
 
-    matmatdist(GridCom, lda, lda, lda, A, B, C, Nglob, Mglob, Pglob, 1, 1, 1, TROW, TCOL);
+    matmatdist(GridCom, ld, ld, ld, A, B, C, N1, N2, N3, 1, 1, 1, TROW, TCOL);
 
     //
     // stampa delle matrici A, B e C
     //
-    for (i=0; i<Nglob/dims[0]; i++){
-        for (j=0; j<Mglob/mcm; j++){
-            printf("MAT A id %d->  %f \n",rank, A[i*lda+j]);
+    /*
+    for (i=0; i<N1/NProw; i++){
+        for (j=0; j<N2/mcm; j++){
+            printf("MAT A id %d (%d,%d)->  %f \n", rank, i, j, A[i*ld+j]);
         }
+        printf("\n");
     }
     printf("------------------\n");
 
-    for (i=0; i<Mglob/mcm; i++){
-        for (j=0; j<Pglob/dims[1]; j++){
-            printf("MAT B id %d->  %f \n",rank, B[i*lda+j]);
+    for (i=0; i<N2/mcm; i++){
+        for (j=0; j<N3/NPcol; j++){
+            printf("MAT B id %d (%d,%d)->  %f \n", rank, i, j, B[i*ld+j]);
         }
+        printf("\n");
     }
     printf("------------------\n");
-    for (i=0; i<Nglob/dims[0]; i++){
-        for (j=0; j<Pglob/dims[1]; j++){
-            printf("MAT C id %d->  %f \n",rank, C[i*lda+j]);
+    for (i=0; i<N1/NProw; i++){
+        for (j=0; j<N3/NPcol; j++){
+            printf("MAT C id %d (%d,%d)->  %f \n", rank, i, j, C[i*ld+j]);
         }
+        printf("\n");
     }
 
     // ==================================================
@@ -114,49 +123,49 @@ int main(int argc, char * argv[]) {
     // ==================================================
 
     srand(0);
-    for(i=0; i<lda; i++){
-        for(j=0; j<lda; j++){
-            *(A+i*lda+j) = (float)rand()/RAND_MAX;
-            *(B+i*lda+j) = (float)rand()/RAND_MAX;
-            *(C+i*lda+j) = (float)rand()/RAND_MAX;
-            *(D+i*lda+j) =  *(C+i*lda+j) ;
+    for(i=0; i<ld; i++){
+        for(j=0; j<ld; j++){
+            *(A+i*ld+j) = (float)rand()/RAND_MAX;
+            *(B+i*ld+j) = (float)rand()/RAND_MAX;
+            *(C+i*ld+j) = (float)rand()/RAND_MAX;
+            *(D+i*ld+j) =  *(C+i*ld+j) ;
         }
     }
 
 
     if(rank==0) printf("               N         time       Gflops\n");
-    for (Nglob = 2048; Nglob <= 2048*3; Nglob = Nglob+2048){
-        Ndouble = Nglob;
+    for (N1 = 2048; N1 <= 2048*3; N1 = N1+2048){
+        Ndouble = N1;
 
         TROW = 1; TCOL =1 ; // test con 1 thread per processo
         MPI_Barrier(MPI_COMM_WORLD);
         time1=get_cur_time();
-        matmatdist(GridCom, lda, lda, lda, A, B, C, Nglob, Nglob, Nglob, 256, 256, 256, TROW, TCOL);
+        matmatdist(GridCom, ld, ld, ld, A, B, C, N1, N1, N1, 256, 256, 256, TROW, TCOL);
         time2=get_cur_time()-time1;
-        printf(" proc = %d:   %4d   %4d   %e  %f \n",rank, Nglob, TROW*TCOL, time2,  2*Ndouble*Ndouble*Ndouble/time2/1.e9);
+        printf(" proc = %d:   %4d   %4d   %e  %f \n",rank, N1, TROW*TCOL, time2,  2*Ndouble*Ndouble*Ndouble/time2/1.e9);
 
         TROW = 2; TCOL =1 ; // test con 2 thread per processo
         MPI_Barrier(MPI_COMM_WORLD);
         time1=get_cur_time();
-        matmatdist(GridCom, lda, lda, lda, A, B, C, Nglob, Nglob, Nglob, 256, 256, 256, TROW, TCOL);
+        matmatdist(GridCom, ld, ld, ld, A, B, C, N1, N1, N1, 256, 256, 256, TROW, TCOL);
         time2=get_cur_time()-time1;
-        printf(" proc = %d:   %4d   %4d   %e  %f \n",rank, Nglob, TROW*TCOL, time2,  2*Ndouble*Ndouble*Ndouble/time2/1.e9);
+        printf(" proc = %d:   %4d   %4d   %e  %f \n",rank, N1, TROW*TCOL, time2,  2*Ndouble*Ndouble*Ndouble/time2/1.e9);
 
         TROW = 2; TCOL =2 ; // test con 4 thread per processo
         MPI_Barrier(MPI_COMM_WORLD);
         time1=get_cur_time();
-        matmatdist(GridCom, lda, lda, lda, A, B, C, Nglob, Nglob, Nglob, 256, 256, 256, TROW, TCOL);
+        matmatdist(GridCom, ld, ld, ld, A, B, C, N1, N1, N1, 256, 256, 256, TROW, TCOL);
         time2=get_cur_time()-time1;
-        printf(" proc = %d:   %4d   %4d   %e  %f \n",rank, Nglob, TROW*TCOL, time2,  2*Ndouble*Ndouble*Ndouble/time2/1.e9);
+        printf(" proc = %d:   %4d   %4d   %e  %f \n",rank, N1, TROW*TCOL, time2,  2*Ndouble*Ndouble*Ndouble/time2/1.e9);
 
         TROW = 4; TCOL =2 ; // test con 8 thread per processo
         MPI_Barrier(MPI_COMM_WORLD);
         time1=get_cur_time();
-        matmatdist(GridCom, lda, lda, lda, A, B, C, Nglob, Nglob, Nglob, 256, 256, 256, TROW, TCOL);
+        matmatdist(GridCom, ld, ld, ld, A, B, C, N1, N1, N1, 256, 256, 256, TROW, TCOL);
         time2=get_cur_time()-time1;
-        printf(" proc = %d:   %4d   %4d   %e  %f \n",rank, Nglob, TROW*TCOL, time2,  2*Ndouble*Ndouble*Ndouble/time2/1.e9);
+        printf(" proc = %d:   %4d   %4d   %e  %f \n",rank, N1, TROW*TCOL, time2,  2*Ndouble*Ndouble*Ndouble/time2/1.e9);
 
-    }
+    }*/
 
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
--- parallel-distributed-computing/hpc/matmatdist/src/matmatdist/matmatdist.c
+++ parallel-distributed-computing/hpc/matmatdist/src/matmatdist/matmatdist.c
@@ -7,7 +7,62 @@
 
 #include "matmatdist.h"
 #include <mpi.h>
+#include <stdio.h>
 
-void matmatdist(MPI_Comm comm, int LDA, int LDB, int LDC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3, int NTROW, int NTCOL) {
-    
+/**
+ @brief This function is called by one or more processes to perform a process-distributed matmatthread.
+ @param comm_grid is a two-dimensional process grid.
+ @param A is part of a global matrix.
+ @param B is part of a global matrix.
+ @param C is part of a global matrix.
+ */
+void matmatdist(MPI_Comm comm_grid, int LDA, int LDB, int LDC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3, int NTROW, int NTCOL) {
+    int ndims = 0;
+    MPI_Cartdim_get(comm_grid, &ndims);
+    if (ndims != 2) {
+        fprintf(stderr, "Number of process grid dimensions (%d) is not equal to 2!\n", ndims);
+        return;
+    }
+
+    int coords[2];
+    get_comm_grid_coords(comm_grid, ndims, coords);
+
+    MPI_Comm comm_row;
+    MPI_Comm comm_col;
+    get_comm_row_col(comm_grid, &comm_row, &comm_col);
+
+    /*int rank_grid = -1;
+    MPI_Comm_rank(comm_grid, &rank_grid);
+    printf("grid_rank %d is (%d, %d)\n", rank_grid, coords[0], coords[1]);*/
+
+    MPI_Comm_free(&comm_row);
+    MPI_Comm_free(&comm_col);
+}
+
+void get_comm_grid_coords(MPI_Comm comm, int ndims, int *coords) {
+    int grid_rank = -1;
+    MPI_Comm_rank(comm, &grid_rank);
+    MPI_Cart_coords(comm, grid_rank, ndims, coords);
+}
+
+void get_comm_row_col(MPI_Comm comm_grid, MPI_Comm *comm_row, MPI_Comm *comm_col) {
+    int remain_dims_row[2] = {0, 1};
+    int remain_dims_col[2] = {1, 0};
+
+    MPI_Cart_sub(comm_grid, remain_dims_row, comm_row);
+    MPI_Cart_sub(comm_grid, remain_dims_col, comm_col);
+}
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
 }
