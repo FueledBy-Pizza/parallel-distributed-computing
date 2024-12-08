b3bf93c76aef42e90e03f78eb459926d1d48f1fb
giuliano <giuliano.aiello.97@gmail.com>
2024-12-08 14:58:55 +0100

    refactor (hpc-matmatdist): delete unnecessary function prototype

--- parallel-distributed-computing/hpc/matmatdist/src/matmatdist/matmatdist.h
+++ parallel-distributed-computing/hpc/matmatdist/src/matmatdist/matmatdist.h
@@ -11,7 +11,6 @@
 #include <mpi.h>
 
 void matmatdist(MPI_Comm comm, int LDA, int LDB, int LDC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3, int NTROW, int NTCOL);
-void get_comm_grid_coords(MPI_Comm comm, int ndims, int *coords);
 void get_comm_row_col(MPI_Comm comm_grid, MPI_Comm *comm_row, MPI_Comm *comm_col);
 int gcd(int a, int b);
 int lcm(int a, int b);
