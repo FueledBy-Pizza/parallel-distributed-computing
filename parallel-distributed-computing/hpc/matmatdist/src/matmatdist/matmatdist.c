45e2b2343158e74c330bdfb84b41147a047cc6bd
giuliano <giuliano.aiello.97@gmail.com>
2024-12-08 13:40:53 +0100

    feat (hpc-matmatdist): implement algorithm (WIP)

--- parallel-distributed-computing/hpc/matmatdist/src/matmatdist/matmatdist.c
+++ parallel-distributed-computing/hpc/matmatdist/src/matmatdist/matmatdist.c
@@ -6,8 +6,10 @@
 //
 
 #include "matmatdist.h"
+#include "../../../matmatthread/src/matmatthread/matmatthread.h"
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  @brief This function is called by one or more processes to perform a process-distributed matmatthread.
@@ -17,6 +19,7 @@
  @param C is part of a global matrix.
  */
 void matmatdist(MPI_Comm comm_grid, int LDA, int LDB, int LDC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3, int NTROW, int NTCOL) {
+
     int ndims = 0;
     MPI_Cartdim_get(comm_grid, &ndims);
     if (ndims != 2) {
@@ -24,25 +27,36 @@ void matmatdist(MPI_Comm comm_grid, int LDA, int LDB, int LDC, double *A, double
         return;
     }
 
-    int coords[2];
-    get_comm_grid_coords(comm_grid, ndims, coords);
+    int *dims    = malloc(sizeof(int) * ndims);
+    int *periods = malloc(sizeof(int) * ndims);
+    int *coords  = malloc(sizeof(int) * ndims);
+    MPI_Cart_get(comm_grid, ndims, dims, periods, coords);
+
+    int NProw = dims [0];
+    int NPcol = dims [1];
+    int mcm = lcm(NProw, NPcol);
 
     MPI_Comm comm_row;
     MPI_Comm comm_col;
     get_comm_row_col(comm_grid, &comm_row, &comm_col);
 
-    /*int rank_grid = -1;
-    MPI_Comm_rank(comm_grid, &rank_grid);
-    printf("grid_rank %d is (%d, %d)\n", rank_grid, coords[0], coords[1]);*/
+    int k;
+    for (k = 0; k < mcm; k++) {
+        int sender_to_row = k % NPcol;
+        int sender_to_col = k % NProw;
+
+        MPI_Bcast(A, N1 / NProw, MPI_DOUBLE, sender_to_row, comm_row);
+        MPI_Bcast(B, N2 / NPcol, MPI_DOUBLE, sender_to_col, comm_col);
+
+        matmatthread(LDA, LDB, LDC, A, B, C, N1, N2, N3, db1, db2, db3, NTROW, NTCOL);
+    }
+
+    free(dims);
+    free(periods);
+    free(coords);
 
     MPI_Comm_free(&comm_row);
     MPI_Comm_free(&comm_col);
-}
-
-void get_comm_grid_coords(MPI_Comm comm, int ndims, int *coords) {
-    int grid_rank = -1;
-    MPI_Comm_rank(comm, &grid_rank);
-    MPI_Cart_coords(comm, grid_rank, ndims, coords);
 }
 
 void get_comm_row_col(MPI_Comm comm_grid, MPI_Comm *comm_row, MPI_Comm *comm_col) {
