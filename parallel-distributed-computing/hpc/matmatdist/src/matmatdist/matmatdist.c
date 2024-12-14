3aebe515b591fb3532426d8d05c63ec0a74e384d
giuliano <giuliano.aiello.97@gmail.com>
2024-12-14 14:33:22 +0100

    refactor (hpc-matmatdist): use lib functions

--- parallel-distributed-computing/hpc/matmatdist/src/matmatdist/matmatdist.c
+++ parallel-distributed-computing/hpc/matmatdist/src/matmatdist/matmatdist.c
@@ -6,78 +6,15 @@
 //
 
 #include "matmatdist.h"
+#include "../../../matmatthread/src/matmatthread/matmatthread.h"
+#include "../../../matmatblock/src/matmatblock/matmatblock.h"
+#include "../../../gemm/src/gemm/gemm.h"
 #include "../../../../common/matrix/matrix.h"
 #include "../../../../common/integer/integer.h"
 #include <omp.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- @brief Copies `A` into `B`.
- */
-/*void copy_matrix_double(double *A, int N_A, int LD_A, double *B, int N_B, int LD_B) {
-    int N, LD, i, j;
-    N = N_B;
-    LD = LD_B;
-
-    for (i = 0; i < N; ++i) {
-        for (j = 0; j < LD; ++j) {
-            B[(i * LD) + j] = A[(i * LD_A) + j];
-        }
-    }
-}*/
-
-/**
- @brief Performs a GeMM operation (`C`=`C`+`A``B`) in the ikj indexes order.
- */
-void matmatikj(int ldA, int ldB, int ldC, double *A, double *B, double *C, int N1, int N2, int N3) {
-    int i, k, j;
-    for (i = 0; i < N1; ++i) {
-        for (k = 0; k < N2; ++k) {
-            for (j = 0; j < N3; ++j) {
-                *(C + (i * ldC) + j) += (*(A + (i * ldA) + k)) * (*(B + (k * ldB) + j));
-            }
-        }
-    }
-}
-
-/**
- @brief In blocks, performs a GeMM operation (`C`=`C`+`A``B`) in the ikj indexes order. "In blocks" means that a `C` block, an `A` block starting from the beginning of the row, a `B` block starting from the beginning of the column are needed (and then passed to matmatikj).
- */
-void matmatblock(int ldA, int ldB, int ldC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3) {
-    int ii, jj, kk;
-    for (ii = 0; ii < N1; ii+=db1) {
-        for (jj = 0; jj < N3; jj+=db3) {
-            for (kk = 0; kk < N2; kk+=db2) {
-                matmatikj(ldA, ldB, ldC, A + (ii * ldA) + kk, B + (kk * ldB) + jj, C + (ii * ldC) + jj, db1, db2, db3);
-            }
-        }
-    }
-}
-
-/**
- @brief Performs a multithreaded matmatblock.
- */
-void matmatthread(int ldA, int ldB, int ldC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3, int NTROW, int NTCOL) {
-    const int n_threads = NTROW * NTCOL;
-    const int submatrix_rows = N1 / NTROW;
-    const int submatrix_cols = N3 / NTCOL;
-
-    omp_set_num_threads(n_threads);
-    #pragma omp parallel
-    {
-        const int tid = omp_get_thread_num();
-
-        const int tid_i = tid / NTCOL;
-        const int tid_j = tid % NTCOL;
-
-        const int submatrix_start_i = tid_i * submatrix_rows;
-        const int submatrix_start_j = tid_j * submatrix_cols;
-
-        matmatblock(ldA, ldB, ldC, A + (submatrix_start_i * ldA), B + submatrix_start_j, C + (submatrix_start_i * ldC) + submatrix_start_j, N1 / NTROW, N2, N3 / NTCOL, db1, db2, db3);
-    }
-}
 
 /**
  @brief This function is called by one or more processes to perform a process-distributed matmatthread.
