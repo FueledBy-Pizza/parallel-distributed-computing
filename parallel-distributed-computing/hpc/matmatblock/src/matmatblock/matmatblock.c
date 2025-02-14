9c6aac263cdc20a87a3db57a839d660f014361f2
Giuliano Aiello <82768767+FueledBy-Pizza@users.noreply.github.com>
2025-02-14 11:23:07 +0100

    refactor

--- parallel-distributed-computing/hpc/matmatblock/src/matmatblock/matmatblock.c
+++ parallel-distributed-computing/hpc/matmatblock/src/matmatblock/matmatblock.c
@@ -13,8 +13,6 @@
  @brief In blocks, performs a GeMM operation (`C`=`C`+`A``B`) in the ikj indexes order. "In blocks" means that a `C` block, an `A` block starting from the beginning of the row, a `B` block starting from the beginning of the column are needed (and then passed to matmatikj).
  */
 void matmatblock(int ldA, int ldB, int ldC, double *A, double *B, double *C, int N1, int N2, int N3, int db1, int db2, int db3) {
-    // doc: draw matrix multiplication by clearly writing the scope of N1, N2, N3, indexes etc. (and the @brief explanation, too -pdf 13, pp.2-)
-    // doc: matmatblock (theoretically) enhances performance only because of cache accesses, so there is no optimal calculation in choosing the best dim_block as a function of N, it only depends on cache, so, and on machine.
     int ii, jj, kk;
     for (ii = 0; ii < N1; ii+=db1) {
         for (jj = 0; jj < N2; jj+=db2) {
